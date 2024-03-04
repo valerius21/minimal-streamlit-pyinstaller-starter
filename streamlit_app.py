07b67722d7b5be83c36f9637a1e5f77b58b11f3a
Valerius Mattfeld <mail@valerius.me>
2024-03-04 16:41:56 +0100

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,12 @@
-import sys
-
 import streamlit as st
-from streamlit.web import cli as stcli
 
 from lib import my_lib
 
 
-def main():
+def run_app():
     my_lib.remove_deploy_button(st)
     st.success('Your streamlit app is running!')
     st.balloons()
 
 
-if __name__ == '__main__':
-    if st.runtime.exists():
-        main()
-    else:
-        sys.argv = [
-            "streamlit",
-            "run",
-            sys.argv[0],
-            # add your own Streamlit command-line options here
-            "--global.developmentMode=false",
-        ]
-        sys.exit(stcli.main())
+run_app()
